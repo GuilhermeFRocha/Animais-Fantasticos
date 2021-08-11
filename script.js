04b22e583436826dcc945b1fb34678a112a3c51a
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2021-08-11 18:21:27 -0300

    Adicionando funcao animacaoScroll

--- script.js
+++ script.js
@@ -46,31 +46,55 @@ function initAccordion() {
 }
 initAccordion();
 
-function initScrollSuave () {
+function initScrollSuave() {
 
-const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
- 
-function scrollToSection (event) {
-  event.preventDefault()
- const href =  event.currentTarget.getAttribute('href')
-const section = document.querySelector(href);
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
 
-section.scrollIntoView ({
-  behavior: 'smooth',
-  block: 'start'
-});
+  function scrollToSection(event) {
+    event.preventDefault()
+    const href = event.currentTarget.getAttribute('href')
+    const section = document.querySelector(href);
 
-// Forma Alternativa
-/* const topo = section.offsetTop;
-window.scrollTo({
-  top: topo,
-  behavior: 'smooth',
-}) */
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+
+    // Forma Alternativa
+    /* const topo = section.offsetTop;
+    window.scrollTo({
+      top: topo,
+      behavior: 'smooth',
+    }) */
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  })
 }
+initScrollSuave();
 
-linksInternos.forEach((link)=>{
-  link.addEventListener('click', scrollToSection);
-})
+function initAnimacaoScroll() {
+
+  const sections = document.querySelectorAll('.js-scroll')
+
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop - windowMetade) < 0;
+
+        if (isSectionVisible) 
+          section.classList.add('ativo')
+         else 
+          section.classList.remove('ativo')
+      })
+    }
+  }
+  animaScroll()
+
+  window.addEventListener('scroll', animaScroll)
 }
-initScrollSuave ();
-
+initAnimacaoScroll()

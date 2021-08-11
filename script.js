076650ed283d013cc0829211be726520794a8c9a
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2021-08-11 16:33:26 -0300

    Adicionando Scroll

--- script.js
+++ script.js
@@ -1,18 +1,76 @@
-const tabmenu = document.querySelectorAll(".js-tabmenu li")
-const tabcontent = document.querySelectorAll(".js-tabcontent section")
+function initTabNav() {
 
-console.log(tabcontent);
+  const tabmenu = document.querySelectorAll(".js-tabmenu li")
+  const tabcontent = document.querySelectorAll(".js-tabcontent section")
 
-function activeTab (index) {
-  tabcontent.forEach((section)=>{
-    section.classList.remove('ativo')
-  })
+  if (tabmenu.length && tabcontent.length) {
 
-  tabcontent[index].classList.add('ativo')
+    tabcontent[0].classList.add("ativo")
+
+    function activeTab(index) {
+      tabcontent.forEach((section) => {
+        section.classList.remove('ativo')
+      })
+
+      tabcontent[index].classList.add('ativo')
+    }
+
+    tabmenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index)
+      })
+    })
+  }
+}
+initTabNav();
+
+function initAccordion() {
+
+  const accordionList = document.querySelectorAll(".js-accordion dt")
+  const activeClass = 'ativo'
+
+  if (accordionList.length) {
+
+    accordionList[0].classList.add(activeClass)
+    accordionList[0].nextElementSibling.classList.add(activeClass)
+
+    function activeAccordion() {
+      this.classList.toggle(activeClass)
+      this.nextElementSibling.classList.toggle(activeClass)
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion)
+    })
+  }
+}
+initAccordion();
+
+function initScrollSuave () {
+
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+ 
+function scrollToSection (event) {
+  event.preventDefault()
+ const href =  event.currentTarget.getAttribute('href')
+const section = document.querySelector(href);
+
+section.scrollIntoView ({
+  behavior: 'smooth',
+  block: 'start'
+});
+
+// Forma Alternativa
+/* const topo = section.offsetTop;
+window.scrollTo({
+  top: topo,
+  behavior: 'smooth',
+}) */
 }
 
-tabmenu.forEach((itemMenu, index)=>{
-  itemMenu.addEventListener("click", ()=>{
-    activeTab(index)
-  })
+linksInternos.forEach((link)=>{
+  link.addEventListener('click', scrollToSection);
 })
+}
+initScrollSuave ();
+

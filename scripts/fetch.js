6b90c3af87e4ff18ab12063eab2da7ab9ab6a893
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2022-01-14 13:56:18 -0300

    adicionando bitcoin

---
+++ scripts/fetch.js
@@ -0,0 +1,12 @@
+// const doc = fetch ('https://viacep.com.br/ws/01001000/json/')
+
+// doc.then(resolucao => {
+//   return resolucao.json();
+
+// }).then((body) =>{
+//   const conteudo = document.querySelector('.titulo');
+//   conteudo.innerText = body.bairro
+// })
+
+// __________________________________________________________________
+
